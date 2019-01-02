751652b916b86712e9bdb03f16eaa3380b45c0e3
Jeremy Simpson <jeremy@adventurekit.app>
2019-01-02 17:16:17 -0500

    Add user model

---
+++ server/src/entities/user.ts
@@ -0,0 +1,86 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  getManager,
+  getRepository,
+  Index,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
+import bcrypt from "bcrypt";
+
+@Entity()
+export default class User {
+
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Index({unique: true})
+  @Column({length: 25, nullable: false})
+  username: string;
+
+  @Index({unique: true})
+  @Column({length: 120, nullable: false})
+  email: string;
+
+  @Column({length: 80, nullable: false})
+  password_hash: string;
+
+  @CreateDateColumn()
+  created?: Date;
+
+  @UpdateDateColumn()
+  updated?: Date;
+
+  @Column({default: false})
+  verified_email?: boolean;
+
+  constructor(username: string, email: string, passwordHash: string) {
+    this.username = username;
+    this.email = email;
+    this.password_hash = passwordHash;
+  }
+
+  static async create(username: string, email: string, password: string): Promise<User> {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const newUser = new User(username, email, passwordHash);
+    return getManager().save(newUser);
+  }
+
+  static async validate(email: string, password: string): Promise<User | undefined> {
+    const user: User | undefined = await User.getByEmail(email);
+
+    if (user === undefined) {
+      return undefined;
+    }
+
+    const verified: boolean = await bcrypt.compare(password, user.password_hash);
+    return verified ? user : undefined;
+  }
+
+  static async getById(id: number): Promise<User | undefined> {
+    return getRepository(User).findOne(id);
+  }
+
+  static async getByUsername(username: string): Promise<User | undefined> {
+    const usernameLower = username.toLowerCase();
+    return getRepository(User)
+      .createQueryBuilder("user")
+      .where("LOWER(user.username) = :usernameLower", {usernameLower: usernameLower})
+      .getOne();
+  }
+
+  static async getByEmail(email: string): Promise<User | undefined> {
+    const emailLower = email.toLowerCase();
+    return getRepository(User)
+      .createQueryBuilder("user")
+      .where("LOWER(user.email) = :emailLower", {emailLower: emailLower})
+      .getOne();
+  }
+
+  async setEmailVerified(status: boolean = true): Promise<User> {
+    this.verified_email = status;
+    return getManager().save(this);
+  }
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,47 @@
-import http from "http";
 import "reflect-metadata"; // Required for typeorm
+import http from "http";
+import {ConnectionOptions, createConnection} from "typeorm";
+
 import {app} from "./app";
 import GameRoomSocketHandler from "./sockets/game_room";
 import config from "./config/config";
 
-const server = new http.Server(app);
+function startServer(): void {
+  const server = new http.Server(app);
 
-// socket server
-new GameRoomSocketHandler(
-  server,
-  config.redis.host,
-  config.redis.port,
-  config.socketIO.redisKey
-).serve();
+  // socket server
+  new GameRoomSocketHandler(
+    server,
+    config.redis.host,
+    config.redis.port,
+    config.socketIO.redisKey
+  ).serve();
 
-// app server
-server.listen(config.web.port, config.web.host, () => {
-  console.log(`Running on http://${config.web.host}:${config.web.port}/`);
-});
+  // app server
+  server.listen(config.web.port, config.web.host, () => {
+    console.log(`Running on http://${config.web.host}:${config.web.port}/`);
+  });
+}
+
+const connectionOptions: ConnectionOptions = {
+  type: "postgres",
+  host: config.postgres.host,
+  port: config.postgres.port,
+  username: config.postgres.username,
+  password: config.postgres.password,
+  database: config.postgres.database,
+  entities: [
+    __dirname + "/entities/*.js"
+  ],
+  migrations: [
+    __dirname + "/migration/*.js"
+  ],
+  synchronize: true,
+  logging: true,
+};
+
+// Database connection
+createConnection(connectionOptions)
+  .then(connection => {
+    startServer();
+  });
