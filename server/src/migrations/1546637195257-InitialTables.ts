2f6e86df6b8f8a08dff9f65465c60b376babb65d
Jeremy Simpson <jeremy@adventurekit.app>
2019-01-29 03:24:06 -0800

    migrations work

--- server/src/migrations/1546637195257-InitialTables.ts
+++ server/src/migrations/1546637195257-InitialTables.ts
@@ -1,7 +1,11 @@
 import {MigrationInterface, QueryRunner} from 'typeorm';
 
-export class AddGameRoom1548751147881 implements MigrationInterface {
+/**
+ * Initial tables migration
+ */
+export class InitialTables1546637195257 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "username" character varying(15) NOT NULL, "email" character varying(120) NOT NULL, "password_hash" character varying(80) NOT NULL, "created" TIMESTAMP NOT NULL DEFAULT now(), "updated" TIMESTAMP NOT NULL DEFAULT now(), "verified_email" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
     await queryRunner.query(`CREATE TABLE "game_room" ("id" SERIAL NOT NULL, "created" TIMESTAMP NOT NULL DEFAULT now(), "updated" TIMESTAMP NOT NULL DEFAULT now(), "ownerId" integer, CONSTRAINT "PK_fa4083cccb79a3e4786a991000b" PRIMARY KEY ("id"))`);
     await queryRunner.query(`CREATE TABLE "game_room_members_user" ("gameRoomId" integer NOT NULL, "userId" integer NOT NULL, CONSTRAINT "PK_6c007274943d6afcd4e5211efd0" PRIMARY KEY ("gameRoomId", "userId"))`);
     await queryRunner.query(`ALTER TABLE "game_room" ADD CONSTRAINT "FK_a87142ca994830b2f09ef55ecbe" FOREIGN KEY ("ownerId") REFERENCES "user"("id")`);
@@ -15,5 +19,6 @@ export class AddGameRoom1548751147881 implements MigrationInterface {
     await queryRunner.query(`ALTER TABLE "game_room" DROP CONSTRAINT "FK_a87142ca994830b2f09ef55ecbe"`);
     await queryRunner.query(`DROP TABLE "game_room_members_user"`);
     await queryRunner.query(`DROP TABLE "game_room"`);
+    await queryRunner.query(`DROP TABLE "user"`);
   }
 }
---
+++ server/src/migrations/1548757978325-PseudoEncrypt.ts
@@ -0,0 +1,41 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+
+/**
+ * We want room IDs to appear "random", but still be unique
+ *
+ * https://wiki.postgresql.org/wiki/Pseudo_encrypt
+ * https://medium.com/@emerson_lackey/postgres-randomized-primary-keys-123cb8fcdeaf
+ */
+export class PseudoEncrypt1548757978325 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`
+      CREATE OR REPLACE FUNCTION pseudo_encrypt(VALUE int) returns int AS $$
+      DECLARE
+      l1 int;
+      l2 int;
+      r1 int;
+      r2 int;
+      i int:=0;
+      BEGIN
+       l1:= (VALUE >> 16) & 65535;
+       r1:= VALUE & 65535;
+       WHILE i < 3 LOOP
+         l2 := r1;
+         r2 := l1 # ((((1366 * r1 + 150889) % 714025) / 714025.0) * 32767)::int;
+         l1 := l2;
+         r1 := r2;
+         i := i + 1;
+       END LOOP;
+       RETURN ((r1 << 16) + l1);
+      END;
+      $$ LANGUAGE plpgsql strict immutable;
+    `);
+
+    await queryRunner.query(`ALTER TABLE game_room ALTER COLUMN id SET DEFAULT pseudo_encrypt(nextval('game_room_id_seq')::int)`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`ALTER TABLE game_room ALTER COLUMN id SET DEFAULT nextval('game_room_id_seq')`);
+    await queryRunner.query(`DROP FUNCTION pseudo_encrypt(VALUE int)`);
+  }
+}
